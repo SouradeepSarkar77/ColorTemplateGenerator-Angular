e07ba9e25b6c84d221e2f8ab4f9eea981703a1bd
Souradeep Sarkar <88938453+SouradeepSarkar77@users.noreply.github.com>
2021-10-08 12:39:53 +0530

    Done

---
+++ src/app/new-color/new-color.component.ts
@@ -0,0 +1,68 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IColor } from '../interfaces/icolor';
+import { ColorModel } from '../models/color-model';
+import { ColorService } from '../services/color.service';
+
+@Component({
+  selector: 'app-new-color',
+  templateUrl: './new-color.component.html',
+  styleUrls: ['./new-color.component.scss']
+})
+export class NewColorComponent implements OnInit {
+  colorData: ColorModel[]
+  logId = 0
+
+  constructor(public colorService: ColorService,
+    private router: Router,
+    private aRoute: ActivatedRoute) {
+    this.colorData = new Array<ColorModel>()
+    this.aRoute.data.subscribe(data => {
+      this.colorData = (data[0] as IColor[]).map( cM => new ColorModel(cM.id, cM.uid, cM.hex_value, cM.color_name))
+    })
+  }
+
+  ngOnInit(): void {
+    this.colorService.color.subscribe(data => {
+      if (data instanceof Array && data) {
+        this.colorData = data
+      } else if (data) {
+        this.colorData.push(data)
+      }
+    })
+  }
+
+  fetchNewColor() {
+    this.colorService.getRandomColor<IColor[]>(5).subscribe(data => {
+      if (data) {
+        this.colorData = data.map( cM => new ColorModel(cM.id, cM.uid, cM.hex_value, cM.color_name))
+        this.colorData.push(new ColorModel(6, 'dfsdf', '#fff123', 'YellowTest'))
+      }
+    })
+  }
+  reloadCurrentPage() {
+    window.location.reload();
+   }
+
+  //  handleKeyboardEvent(event: KeyboardEvent) {
+  //   switch (event.key) {
+  //     case 'Space':
+  //       // trigger something from the left arrow
+  //       break;
+  //     case 'ArrowRight':
+  //       // trigger something from the right arrow
+  //   }
+  // }
+
+  // onKeydown(event: any) {
+  //   if (event.key === "Enter") {
+  //     console.log(event);
+  //   }
+  // }
+
+  // @HostListener('document:keypress', ['$event'])
+  // handleKeyboardEvent(event: KeyboardEvent) {
+  //   this.key = event.key;
+  // }
+
+}
---
+++ src/app/services/color.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ColorService implements OnDestroy {
+  color = new BehaviorSubject<any>(null)
+  constructor(private httpClient : HttpClient) {
+
+   }
+  ngOnDestroy(): void {
+    this.color.unsubscribe();
+  }
+
+  getRandomColor<T>(size: number = 0): Observable<T>{
+    let url = `https://random-data-api.com/api/color/random_color`;
+    const finalUrl = size ? `${url}?size=${size}` : url;
+   return this.httpClient.get<T>(finalUrl);
+  }
+}
+
